aa2740f33c295a211756ce3d2ea9237785d9fc18
p775 <P775_wsl@Ubuntu22.04>
2025-05-14 08:53:32 +0800

    first concepts example

--- c++20/01_Concepts/example_01/main.cpp
+++ c++20/01_Concepts/example_01/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <concepts>
+#include <type_traits>
 
-template<std::arithmetic T>
+template<typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
+template<Arithmetic T>
 T sum(T a, T b) {
     return a + b;
 }
 
 int main() {
-    std::cout << sum(3, 4) << "\n";  // OK: int
-    std::cout << sum(3.5, 2.1) << "\n"; // OK: double
+    std::cout << sum(3, 4) << "\n";       // OK: int
+    std::cout << sum(3.5, 2.1) << "\n";   // OK: double
+    // std::cout << sum("hi", "there");  // Compile-time error
     return 0;
 }
-
